4223d97c1fe7fb58e14ead24d4a5e00279d5628b
raihaanshaarif <codexbd@gmail.com>
2025-03-20 20:21:15 +0600

    set cookies in the browser and login return

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,40 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
+
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues:{
+      userId: '0001',
+      password: 'admin12345'
+    }
+  })
+
+  const [login, {data, error}] = useLoginMutation()
+  console.log('Data =>', data)
+  console.log('Error =>', error)
+
+  const onSubmit = (data: { userId: string; password: string }) => {
+    const userInfo= {
+      id: data.userId,
+      password: data.password
+    }
+    login(userInfo)
+  }
   return (
-    <div>
-      <h1>This is a Login Page</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id='id' {...register('userId')}/>
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id='password'  {...register('password')}/>
+      </div>
+      <Button htmlType="submit">Submit</Button>
+    </form>
   );
 };
 
